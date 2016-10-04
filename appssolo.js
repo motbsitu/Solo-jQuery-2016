32cb495184aac8faba0a475c5f4f5ef9ea04bbd7
Julie S Mike <juliesmike@gmail.com>
2016-10-03 21:03:22 -0500

    Got it working

--- appssolo.js
+++ appssolo.js
@@ -4,41 +4,60 @@ $(document).ready(function() {
     var countGreen = 0;
     var countBlue = 0;
 
-    $('.color-button').click(function() {
+
+    $(':button').click(function(event) {
+          var buttonClicked = $(this);
+
         console.log('Some button was clicked');
 
-        createBoxes();
-
-
-
+        createBoxes(buttonClicked);
+        countColorClick(buttonClicked);
     });
 
 //function to create boxes, assigned specific color
 //based on button click
-    function createBoxes() {
+    function createBoxes(buttonClicked) {
 
         var $box = $('<div></div>');
+
+        if(buttonClicked.attr('data-color') == "red"){
+          $box.addClass('red');
+        }
+        if(buttonClicked.attr('data-color') == "yellow"){
+          $box.addClass('yellow');
+        }
+        if(buttonClicked.attr('data-color') == "green"){
+          $box.addClass('green');
+        }
+        if(buttonClicked.attr('data-color') == "blue"){
+          $box.addClass('blue');
+        }
         $box.addClass('color-cube');
 
         $('.container').append($box);
 
     }
 
-//function to associate box color to create with button clicked
-  function boxColor(){
-    if(data-color == ){
-      
-    }
-  })
-  //   function updateDOM() {
-  //   $(p id = 'red').html(countRed);
-  // }
-
-
   //function to count
-  countRed++;
-  countYellow++;
-  countGreen++;
-  countBlue++;
+    function countColorClick(buttonClicked){
+      if(buttonClicked.attr('data-color') == "red"){
+        countRed++;
+        console.log(countRed);
+        $("p[id=red]").text('Total Red: ' + countRed);
+      }
+      if(buttonClicked.attr('data-color') == "yellow"){
+        countYellow++;
+        $("p[id=yellow]").text('Total Yellow: ' + countYellow);
+      }
+      if(buttonClicked.attr('data-color') == "green"){
+        countGreen++;
+        $("p[id=green]").text('Total Green: ' + countGreen);
+      }
+      if(buttonClicked.attr('data-color') == "blue"){
+        countBlue++;
+        $("p[id=blue]").text('Total Blue: ' + countBlue);
+      }
+
+}
 
 });
